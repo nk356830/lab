c43b0b8b3c2710e8a39a6fe4fb7701df8d2e84fd
nk356830 <nk356830@dal.ca>
2020-05-12 19:42:35 -0300

    Task1 Commited

--- task1.py
+++ task1.py
@@ -2,7 +2,7 @@ import string
 mybook=open("book.txt","r")
 lines=mybook.readlines()
 for i in lines:
-  a=i.split(' ')
+  a=i.split()
   for j in a:
     b=j.strip(string.punctuation)
     print(b.lower())
