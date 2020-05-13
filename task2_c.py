30a76457addcad587418c11f41d0acefac6dba9d
nk356830 <nk356830@dal.ca>
2020-05-13 00:48:17 -0300

    task 2 c completed

---
+++ task2_c.py
@@ -0,0 +1,16 @@
+import string
+freq={}
+mybook=open("book.txt","rt")
+lines=mybook.readlines()
+counter = 0
+for i in lines:
+  a=i.split()
+  for j in a: 
+    counter += 1
+    b=j.strip(string.punctuation)
+    if b in freq.keys():
+      freq[b]= freq[b] + 1
+    else:
+      freq[b]=1
+print(len(freq.keys()))
+print(freq.keys())
